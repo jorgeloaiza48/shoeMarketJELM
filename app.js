85965d64d512612057eec9b5c01f6a847adc5a63
joeecasa <joeecasa@gmail.com>
2022-04-07 14:04:46 -0300

    index

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ app.use(express.static("public"));
 // app.use("/static",express.static(__dirname,))
 
 app.get ("/",(req, res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+    res.sendFile(path.join(__dirname,"/views/index.html"))
 
 })
 
 app.get ("/carritodecompras",(req, res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+    res.sendFile(path.join(__dirname,"/views/CarritoDeCompras.html"))
 })
 
 app.get ("/login",(req, res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+    res.sendFile(path.join(__dirname,"/views/login.html"))
 })
 
 app.get ("/registro",(req, res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+    res.sendFile(path.join(__dirname,"/views/Registro.html"))
 })
 
 app.get ("/descripciondelproducto",(req, res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+    res.sendFile(path.join(__dirname,"/views/DescripcionDelProducto.html"))
 })
 
