c96c726341621e9afb13ded061911e7e7b97017c
jorgeloaiza48 <jelm48@misena.edu.co>
2022-04-25 15:03:51 -0500

    Organizando enlaces para 360px

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.get('/',(req, res) =>{
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
 })
 
-app.get('/carritodecompras',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/carritodecompras.html'))
+app.get('/carrito',(req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/carrito.html'))
 })
 
 app.get('/login',(req, res) =>{
@@ -31,11 +31,6 @@ app.get('/registro',(req, res) =>{
     res.sendFile(path.resolve(__dirname,'./views/registro.html'))
 })
 
-<<<<<<< HEAD
-app.get('/descripciondelproducto',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/descripciondelproducto.html'))
-=======
-app.get("/descripcion",(req, res) =>{
-    res.sendFile(path.join(__dirname,"/views/descripcion.html"))
->>>>>>> eb53b5e5520e4df012ccb64c9367ff42f7c4a2e9
+app.get('/descripcion',(req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/descripcion.html'))
 })
