3bbdcd347bbe33adeac7735a4a22aee441f51ef2
joeecasa <joeecasa@gmail.com>
2022-04-25 13:52:49 -0300

    s

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/',(req, res) =>{
 })
 
 app.get('/carritodecompras',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/carritodecompras.html'))
+    res.sendFile(path.resolve(__dirname,'./views/carrito.html'))
 })
 
 app.get('/login',(req, res) =>{
@@ -31,11 +31,6 @@ app.get('/registro',(req, res) =>{
     res.sendFile(path.resolve(__dirname,'./views/registro.html'))
 })
 
-<<<<<<< HEAD
-app.get('/descripciondelproducto',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/descripciondelproducto.html'))
-=======
 app.get("/descripcion",(req, res) =>{
     res.sendFile(path.join(__dirname,"/views/descripcion.html"))
->>>>>>> eb53b5e5520e4df012ccb64c9367ff42f7c4a2e9
 })
