98102d5f495deab6a807eeff2feb216792cf995f
joeecasa <joeecasa@gmail.com>
2022-04-25 14:16:24 -0300

    s

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-const puerto = 4000
 
-
+let puerto = 4000
 app.listen(process.env.PORT || puerto, ()=>console.log("Servidor corriendo en el puerto ---> " + puerto))
 
 //configuración para montar en Heroku
 
-// const publicPath = path.resolve(__dirname,'./public')
-// app.use(express.static(publicPath))
+ const publicPath = path.resolve(__dirname,'./public')
+  app.use(express.static(publicPath))
 
-const publicPath = path.resolve(__dirname,'./public')
-app.use(express.static(publicPath));
+app.use(express.static("public"));
 
 // app.use("/static",express.static(__dirname,))
 
-app.get('/',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,"/views/home.html"))
+app.get("/",(req, res) =>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
 })
 
-app.get('/carrito',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,"/views/carrito.html"))
+app.get("/carritodecompras",(req, res) =>{
+    res.sendFile(path.join(__dirname,"/views/carrito.html"))
 })
 
-app.get('/login',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,"/views/login.html"))
+app.get("/login",(req, res) =>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
 })
 
-app.get('/registro',(req, res) =>{
-    res.sendFile(path.resolve(__dirname,"/views/registro.html"))
+app.get("/registro",(req, res) =>{
+    res.sendFile(path.join(__dirname,"/views/registro.html"))
 })
 
-app.get("/descripcion",(req, res) =>{
+app.get("/descripciondelproducto",(req, res) =>{
     res.sendFile(path.join(__dirname,"/views/descripcion.html"))
 })
