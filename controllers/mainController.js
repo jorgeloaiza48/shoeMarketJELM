8007d9d23e5a6885dd316a2f3706a54b538b750e
joeecasa <joeecasa@gmail.com>
2022-05-09 14:35:54 -0300

    s

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,17 +3,17 @@
 const controller = {
     index:(req,res) =>{res.render("home", {title: "Shoe Market"})},
 
-    register: (req,res) => { res.render("registro", {title: "Registro"} )},
+    register: (req,res) => { res.render("users/registro", {title: "Registro"} )},
 
-    login:(req,res) => {res.render('/users/login',{title: "Login"})},
+    login:(req,res) => {res.render('users/login',{title: "Login"})},
 
-    descripcion:(req,res) => {res.render('descripcion',{title: "Descripcion"})},
+    descripcion:(req,res) => {res.render('products/descripcion',{title: "Descripcion"})},
 
-    carrito:(req,res) => {res.render('carrito',{title: "Carrito de compras"})},
+    carrito:(req,res) => {res.render('products/carrito',{title: "Carrito de compras"})},
 
-    crearProducto: (req,res) => {res.render('crearProducto',{title: "Crear Producto"})},
+    crearProducto: (req,res) => {res.render('products/crearProducto',{title: "Crear Producto"})},
 
-    editarProducto: (req,res) => {res.render('editarProducto',{title: "Editar producto"})}
+    editarProducto: (req,res) => {res.render('products/editarProducto',{title: "Editar producto"})}
 
 
 }
