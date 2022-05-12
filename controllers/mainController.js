6f1beac8b5b9c176eea1d35828ac009b53cbbefe
joeecasa <joeecasa@gmail.com>
2022-05-12 12:26:10 -0300

    json

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,19 +1,62 @@
+const req = require("express/lib/request")
+const res = require("express/lib/response")
+const path = require("path")
+const fs = require("fs")
 
 
 const controller = {
-    index:(req,res) =>{res.render("home", {title: "Shoe Market"})},
+    index: (req, res) => { res.render("home", { title: "Shoe Market" }) },
 
-    register: (req,res) => { res.render("users/registro", {title: "Registro"} )},
+    register: (req, res) => { res.render("users/registro", { title: "Registro" }) },
 
-    login:(req,res) => {res.render('users/login',{title: "Login"})},
+    login: (req, res) => { res.render('users/login', { title: "Login" }) },
 
-    descripcion:(req,res) => {res.render('products/descripcion',{title: "Descripcion"})},
+    descripcion: (req, res) => { res.render('products/descripcion', { title: "Descripcion" }) },
 
-    carrito:(req,res) => {res.render('products/carrito',{title: "Carrito de compras"})},
+    carrito: (req, res) => { res.render('products/carrito', { title: "Carrito de compras" }) },
 
-    crearProducto: (req,res) => {res.render('products/crearProducto',{title: "Crear Producto"})},
+    crearProducto: (req, res) => { res.render('products/crearProducto', { title: "Crear Producto" }) },
 
-    editarProducto: (req,res) => {res.render('products/editarProducto',{title: "Editar producto"})}
+    editarProducto: (req, res) => { res.render('products/editarProducto', { title: "Editar producto" }) },
+
+    products: (req, res) => {
+        //para estos 3 no se puede usar const ya que mas abajo cuando reescribamos los json hay q volver a declarar las variables
+        // y las variables declaradas con const no se pueden modificar
+        let productsFilePath = path.join(__dirname, '../data/SHOEMARKET.json');
+        let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        let productJSON = fs.readFileSync(productsFilePath, 'utf-8')
+        let botas = products.filter(function (product) {
+            return product.category == "Botas";
+        })
+        let texanas = products.filter(function (product) {
+            return product.category == "Texanas";
+        })
+        let bucaneras = products.filter(function (product) {
+            return product.category == "Bucaneras";
+        })
+        let borcegos = products.filter(function (product) {
+            return product.category == "Borcegos";
+        })
+        let guillerminas = products.filter(function (product) {
+            return product.category == "Gillerminas";
+        })
+        let giftCard = products.filter(function (product) {
+            return product.category == "GiftCard";
+        })
+
+
+
+
+        res.render("products/productos", {
+            products: products,
+            botas: botas,
+            texanas: texanas,
+            bucaneras: bucaneras,
+            borcegos: borcegos,
+            guillerminas: guillerminas,
+            title : "Productos"
+        })
+    }
 
 
 }
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,11 +4,19 @@ const router = express.Router()
 const mainController = require('../controllers/mainController')
 
 router.get('/',mainController.index)
+
+router.get("/productos", mainController.products)
+
 router.get('/registro', mainController.register)
+
 router.get('/login',mainController.login)
+
 router.get('/descripcion',mainController.descripcion)
+
 router.get('/carrito',mainController.carrito)
+
 router.get("/crearproducto", mainController.crearProducto)
+
 router.get("/editarproducto",mainController.editarProducto)
 
 module.exports = router
