9e6073b3e04059b75791774fb1b290ca40cb7c3c
joeecasa <joeecasa@gmail.com>
2022-05-24 13:21:21 -0300

    s

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const path = require("path")
 const {body} = require('express-validator')
+//const userController = require('../controllers/userController')
 const userController = require('../controllers/userController')
 const multer = require("multer")
 
