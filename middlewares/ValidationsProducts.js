34ab41853cff88f0d0c1970803844cf57ad8879d
joeecasa <joeecasa@gmail.com>
2022-06-06 19:56:27 -0300

    reparando

--- middlewares/ValidationsProducts.js
+++ middlewares/ValidationsProducts.js
@@ -1,26 +1,12 @@
 const {body} = require('express-validator')
 
 const ValidationsProducts = [
-    body('nombre', "Debe introducir un nombre de producto").notEmpty(),
+    body('nombre', "Debe introducir un nombre").notEmpty(),
     body("price","Debe introducir un precio").notEmpty(),
+    
     body("color", " Debe seleccionar al menos un color").exists(), 
     body("size", " Debe seleccionar al menos un talle").exists(), 
-    body("category", " Debe seleccionar una categoria").exists(),  
-    body('photo').custom((value,{req})=>{
-        let file = req.file
-        let extensionesAceptadas = ['.jpg','.png','.gif','.png']
-        
-        if(!file){
-          throw new Error('Tiene que seleccionar una imagen como foto de producto.')
-        } else{
-          let fileExtension = path.extname(file.originalname)
-          if (!extensionesAceptadas.includes(fileExtension)){
-            throw new Error ('Las extensiones permitidas son ' + extensionesAceptadas)
-        }
-      }
-        return true
-      })
-      
+    body("category", " Debe seleccionar una categoria").exists(),      
 ]
 
 module.exports = ValidationsProducts
