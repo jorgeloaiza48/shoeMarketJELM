a20bbf9f273313db56afd3572a13b34d29a775f1
joeecasa <joeecasa@gmail.com>
2022-07-01 19:40:53 -0300

    s

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "Joelcasa2022",
-    "database": "shoemarket",
-    "host": "127.0.0.1",
+    "username": "grupo4fourcoding",
+    "password": "grupo4fourcoding",
+    "database": "shoemarket4",
+    "host": "db4free.net",
     //"port" : "3306",/// el 3306 es el servidor por defecto x eso no es necesario pero si usamos otro si es necesario
     "dialect": "mysql"
   },
