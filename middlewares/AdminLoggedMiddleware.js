f2c67193d0940a11b79230b709b32843261fac91
joeecasa <joeecasa@gmail.com>
2022-07-12 23:52:56 -0300

    heroku no anda

--- middlewares/AdminLoggedMiddleware.js
+++ middlewares/AdminLoggedMiddleware.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const usersFilePath = path.join(__dirname, '../data/users.json');
+
 
 const adminLoggedMiddleware = (req, res, next) => {
     res.locals.isAdmin = req.session.isAdmin;
