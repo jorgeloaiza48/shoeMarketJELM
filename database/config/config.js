66e8c162ea4806ad6c39ebfe01429b4139139c0b
joeecasa <joeecasa@gmail.com>
2022-07-13 00:10:40 -0300

    ssas

--- database/config/config.js
+++ database/config/config.js
@@ -4,6 +4,21 @@ module.exports = {
     "password": "q0accczyukdbzzrl", //AvapkRmZXc
     "database": "rnj9ky0w22h0cycz",           //rdPFvYiYwX
     "host"    : "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",               //remotemysql.com
-    "dialect": "mariadb"
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "Joelcasa2022",
+    "database": "shoemarket",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "grupo4fourcoding",
+    "password": "grupo4fourcoding",
+    "database": "shoemarket4",
+    "host": "db4free.net",
+    //"port" : "3306",/// el 3306 es el servidor por defecto x eso no es necesario pero si usamos otro si es necesario
+    "dialect": "mysql"
   }
 }
