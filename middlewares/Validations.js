e5a2fe1b3f457c36d37ecca8e8674ed3ad080f43
jorgeloaiza48 <jelm48@misena.edu.co>
2022-07-13 10:28:25 -0500

    Mejorando las validaciones de users

--- middlewares/Validations.js
+++ middlewares/Validations.js
@@ -3,7 +3,7 @@ const {body} = require('express-validator')
 const path = require('path')
 
 const validations = [
-    body('documento').notEmpty().withMessage('Debe introducir un número de identificación').bail().isLength({min:5}).withMessage('El número de identificación debe ser de mínimo cinco números'),
+    body('documento').notEmpty().withMessage('Debe introducir un número de identificación').bail().isLength({min:5,max:10}).withMessage('El número de identificación debe ser de mínimo cinco números y máximo de 10'),
     body('nombre').notEmpty().withMessage('Debe introducir un nombre y apellido').bail().isLength({min:2}).withMessage('El nombre debe tener por lo menos de 2 caracteres'),
     body('apellido').notEmpty().withMessage('Debe introducir un apellido').bail().isLength({min:2}).withMessage('El apellido debe tener por lo menos de 2 caracteres'),
     body('email').notEmpty().withMessage('Debe introducir un correo electrónico').bail().isEmail().withMessage('Debe ingresar una dirección de correo válida'),
