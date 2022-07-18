bc998669abb479b6876cb884c05254043c7185d3
joeecasa <joeecasa@gmail.com>
2022-07-17 22:26:40 -0300

    registro front

--- public/js/registro.js
+++ public/js/registro.js
@@ -1,4 +1,4 @@
-window.onload = function (){
+window.onload = function () {
     let form = document.querySelector(".form-Reg")
     let documento = document.querySelector("#documento");
     let nombre = document.querySelector("#nombre");
@@ -14,52 +14,34 @@ window.onload = function (){
     let errorNombre = document.querySelector(".errorNombre")
 
 
-// form.addEventListener("submit",function(event){
-//     event.preventDefault();
-//     console.log(document.forms)
-//     ;
-//     for (let i = 0; i < documentForm.elements.length; i++) {
-//        if(documentForm.elements[i].value === ""){
-//         documentForm.elements[i].classList.add("is-invalid")
-//         errors.push(`El campo ${documentForm.elements[i].name} no debe estar vacio`)
+    nombre.addEventListener("change", function (event) {
+        let liErrorNombre1 = document.querySelector("#liErrorNombre1")
+        let liErrorNombre2 = document.querySelector("#liErrorNombre2")
 
-//        } else {
-//         documentForm.elements[i].classList.remove("is-invalid")
-//        }
-        
-//     }
+        if (nombre.value === "") {
+            liErrorNombre1.classList.remove("none")
+            liErrorNombre1.classList.add("show")
+            nombre.classList.add("error")
+        } else {
+            liErrorNombre1.classList.add("none")
+            nombre.classList.remove("error")
+            
+        }
+        if (nombre.value.length < 2) {
+            liErrorNombre2.classList.remove("none")
+            liErrorNombre2.classList.add("show")
+            nombre.classList.add("error")
+        } else {
+            liErrorNombre2.classList.add("none")
+            nombre.classList.remove("error")
+          
 
 
-    // let ulErrores = document.querySelector(".errores")
-    // console.log(errors)
+        }
 
-    //         errors.forEach(function (error) {
-    //             ulErrores.innerHTML += "<li>" + error + "<li>"
-
-            // })
-// })
-
-nombre.addEventListener("blur",function(event){
-    event.preventDefault()
-    let errorsNombre = []
-    if(nombre.value === ""){
-        errorsNombre.push("No debe estar vacio")
-    }
-    if(nombre.value.length <= 2){
-        errorsNombre.push("El nombre y apellido debe ser mayor a 2")
-    } else {
-        errorsNombre = []
-    }
-    errorsNombre.forEach(function(error){
-        console.log(error)
-
-        errorNombre.innerHTML += "<li>" + error + "</li>"
     })
 
 
-})
 
-
-    
 
 }
