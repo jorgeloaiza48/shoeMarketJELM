4fe707963d1acd531248d9843ddb9b30c20ed523
jorgeloaiza48 <jelm48@misena.edu.co>
2022-08-07 15:56:39 -0500

    redondeando imágenes de usuarios

--- routes/ApiRoutes/ApiMainRoutes.js
+++ routes/ApiRoutes/ApiMainRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const ApiProductsRoutes = require("./ApiProductsRoutes")
+const ApiUsersRoutes = require("./ApiUsersRoutes")
 
 
 //** router de productos **//
