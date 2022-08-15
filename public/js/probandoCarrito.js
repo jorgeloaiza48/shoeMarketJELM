940631f679fcd855389263b52a78ece47b5ddc7e
joeecasa <joeecasa@gmail.com>
2022-08-15 15:44:15 -0300

    sumando el total

--- public/js/probandoCarrito.js
+++ public/js/probandoCarrito.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("load", function (event) {
 
     let probandoCarrito = document.querySelector(".probandoCarrito")
@@ -9,21 +10,20 @@ window.addEventListener("load", function (event) {
     let totalPrice = document.querySelector(".contenedor-total__price")
     let filaCarritoPrice = document.querySelector("#hola")
     let btnDeleteCarrito = document.querySelector("btn-delete-carrito")
-    console.log(btnDeleteCarrito)
-
 
 
     if (local) {
 
         local.forEach(producto => {
             tablaCarrito.innerHTML +=
-            `<tr class="filaCompleta-carrito" >` +
+                `<tr class="filaCompleta-carrito" >` +
                 `<td class="fila-carrito">` + `<img class="img-carrito" src=${producto.imagen} >` + "</img>" + "</td>" +
                 `<td class="fila-carrito">` + producto.nombre + "</td>" +
                 `<td class="fila-carrito">` + producto.cantidad + "</td>" +
                 `<td class="fila-carrito-price">` + "$" + producto.precio * producto.cantidad + "</td>" +
                 `<td><a href="#" class="borrar-curso" data-id="${producto.id}">X</a> </td>`
             "</tr>"
+
 
 
         });
@@ -40,7 +40,21 @@ window.addEventListener("load", function (event) {
 
     }
 
-    console.log(filaCarritoPrice)
+    let td = document.querySelectorAll(".fila-carrito-price")
+    let arrayPrecios = []
+    let total = 0
+    if (td) {
+        td.forEach((producto) => {
+            let precio = parseInt(producto.outerText.replace("$", "0"))
+            total += precio
+        })
+    }
+    
+    totalPrice.innerHTML += "$ " + total
+
+
+
+
 
 
 
