cc971974264a602ecf3c4d22ee41841f7fb2fa22
jorgeloaiza48 <jelm48@misena.edu.co>
2022-08-31 15:23:19 -0500

    fdfds

--- database/config/config.js
+++ database/config/config.js
@@ -24,6 +24,32 @@ module.exports = {
   }
 }
 
+// **********************Conexión remotemysql************************
+
+// module.exports = {
+//   "development": {
+//     "username": "WArbTvEJfU",   
+//     "password": "cvlXSAef5M", 
+//     "database": "WArbTvEJfU",           
+//     "host"    : "remotemysql.com",              
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "WArbTvEJfU",   
+//     "password": "cvlXSAef5M",
+//     "database": "WArbTvEJfU",           
+//     "host"    : "remotemysql.com",              
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "WArbTvEJfU",   
+//     "password": "cvlXSAef5M", 
+//     "database": "WArbTvEJfU",           
+//     "host"    : "remotemysql.com",               
+//     "dialect": "mysql"
+//   }
+// }
+
 
 //********************** Conexión para Maria **********************/
 
@@ -32,21 +58,21 @@ module.exports = {
 //     "username": "c28sqwv991td8shu",   //rdPFvYiYwX
 //     "password": "q0accczyukdbzzrl", //AvapkRmZXc
 //     "database": "rnj9ky0w22h0cycz",           //rdPFvYiYwX
-//     "host"    : "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",               //remotemysql.com
+//     "host"    : "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",               
 //     "dialect": "mysql"
 //   },
 //   "test": {
 //     "username": "c28sqwv991td8shu",   //rdPFvYiYwX
 //     "password": "q0accczyukdbzzrl", //AvapkRmZXc
 //     "database": "rnj9ky0w22h0cycz",           //rdPFvYiYwX
-//     "host"    : "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",               //remotemysql.com
+//     "host"    : "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",               
 //     "dialect": "mysql"
 //   },
 //   "production": {
 //     "username": "c28sqwv991td8shu",   //rdPFvYiYwX
 //     "password": "q0accczyukdbzzrl", //AvapkRmZXc
 //     "database": "rnj9ky0w22h0cycz",           //rdPFvYiYwX
-//     "host"    : "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",               //remotemysql.com
+//     "host"    : "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",               
 //     "dialect": "mysql"
 //   }
 // }
