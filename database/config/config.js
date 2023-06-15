031eab266bb157f8c03d5dc80324d765e2ec8aff
jorgeloaiza48 <jelm48@misena.edu.co>
2023-06-15 09:54:28 -0500

    ++--

--- database/config/config.js
+++ database/config/config.js
@@ -1,54 +1,54 @@
 // **********************Conexión local************************
 
-module.exports = {
-  "development": {
-    "username": "root",   
-    "password": "George4810008968", 
-    "database": "rnj9ky0w22h0cycz",           
-    "host"    : "localhost",              
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",   
-    "password": "George4810008968",
-    "database": "rnj9ky0w22h0cycz",           
-    "host"    : "localhost",              
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",   
-    "password": "George4810008968", 
-    "database": "rnj9ky0w22h0cycz",           
-    "host"    : "localhost",               
-    "dialect": "mysql"
-  }
-}
-
-// **********************Conexión sql10.freesqldatabase.com ************************
-
 // module.exports = {
 //   "development": {
-//     "username": "sql10516464",   
-//     "password": "4jJX98pdiw", 
-//     "database": "sql10516464",           
-//     "host"    : "sql10.freesqldatabase.com",              
+//     "username": "root",   
+//     "password": "George4810008968", 
+//     "database": "rnj9ky0w22h0cycz",           
+//     "host"    : "localhost",              
 //     "dialect": "mysql"
 //   },
 //   "test": {
-//     "username": "sql10516464",   
-//     "password": "4jJX98pdiw",
-//     "database": "sql10516464",           
-//     "host"    : "sql10.freesqldatabase.com",              
+//     "username": "root",   
+//     "password": "George4810008968",
+//     "database": "rnj9ky0w22h0cycz",           
+//     "host"    : "localhost",              
 //     "dialect": "mysql"
 //   },
 //   "production": {
-//     "username": "sql10516464",   
-//     "password": "4jJX98pdiw", 
-//     "database": "sql10516464",           
-//     "host"    : "sql10.freesqldatabase.com",               
+//     "username": "root",   
+//     "password": "George4810008968", 
+//     "database": "rnj9ky0w22h0cycz",           
+//     "host"    : "localhost",               
 //     "dialect": "mysql"
 //   }
 // }
+
+// **********************Conexión sql10.freesqldatabase.com ************************
+
+module.exports = {
+  "development": {
+    "username": "sql10516464",   
+    "password": "4jJX98pdiw", 
+    "database": "sql10516464",           
+    "host"    : "sql10.freesqldatabase.com",              
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "sql10516464",   
+    "password": "4jJX98pdiw",
+    "database": "sql10516464",           
+    "host"    : "sql10.freesqldatabase.com",              
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "sql10516464",   
+    "password": "4jJX98pdiw", 
+    "database": "sql10516464",           
+    "host"    : "sql10.freesqldatabase.com",               
+    "dialect": "mysql"
+  }
+}
 
 
 //********************** Conexión para Maria **********************/
